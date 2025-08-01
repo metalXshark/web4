2ebda9a308f44134cf37a767b9eff26243a9b556
kj.kir <125216868+metalXshark@users.noreply.github.com>
2025-08-01 20:08:50 +0300

    Update Header.js

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -4,7 +4,7 @@ function Header() {
     return (
         <header className="header">
             <div>
-                <span>Nikita Kobik, P3224</span>
+                <span>Кирилл Кононыхин, P3224</span>
                 <span>variant 412346</span>
             </div>
         </header>
